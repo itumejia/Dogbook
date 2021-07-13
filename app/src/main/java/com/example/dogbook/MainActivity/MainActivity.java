524c3a50baa54915e880ccdedac2d99e019bd857
itumejia <iturielmg@hotmail.com>
2021-07-13 18:24:02 -0500

    Added toolbar and bottom navigation bar to main activity

--- app/src/main/java/com/example/dogbook/MainActivity/MainActivity.java
+++ app/src/main/java/com/example/dogbook/MainActivity/MainActivity.java
@@ -1,16 +1,52 @@
 package com.example.dogbook.MainActivity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import com.example.dogbook.LoginActivity.LoginActivity;
 import com.example.dogbook.R;
+import com.parse.ParseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+    }
+
+    private void initView() {
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("");
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main_activity, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.itemLogOut) {
+            ParseUser.logOutInBackground();
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
